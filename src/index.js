d8cd5ef36be897e853a91c0f426498cd13003b40
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2024-04-26 00:02:56 +0700

    feat:initialize basic structure

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+const cors = require('cors');
+
+const express = require("express");
+const app = express();
+
+dotenv.config();
+
+//* Enabling cors for all request by usiing cors middleware
+app.use(cors({
+    origin: "*"
+}));
+
+/**
+ * * Parse request of content-type: application/json
+ * * Parses inconming request with JSON payloads
+ */
+app.use( express.json());
+app.use( express.urlencoded( { extended:true } ) );
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('You have invalid JSON Format Type On Request Parameter')
+});
+
+const prefix = process.env.PREFIX 
+
+// API endpoint
+app.get(`/${prefix}`, (req, res) => {
+    res.json({
+        message: "API is working!"
+    })
+})
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:" + port + "\t" + process.env.NODE_ENV);
+});
