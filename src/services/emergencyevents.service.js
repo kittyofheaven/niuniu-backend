c013ef5b29f00e165fd5c8feccecd2413a0cf116
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2024-06-28 17:37:14 +0700

    feat: add hospital name on update hospital emergency events id

--- src/services/emergencyevents.service.js
+++ src/services/emergencyevents.service.js
@@ -350,6 +350,7 @@ const updateEmergencyTypeEmergencyEvent = async (driver_id, emergency_id, emerge
                         user_id: user_id,
                         user_name: user_name,
                         user_location: user_location,
+                        hospital_name: nearestHospital.hospital_name,
                         hospital_location: nearestHospital.location,
                         emergency_type: emergency_type,
                         number_of_patient: emergencyEvent.number_of_patient,
